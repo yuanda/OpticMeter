d1e7fbb9db8d10966bb7ea660953403a1c334659
yuanda <yuandahe1@gmail.com>
2014-02-19 06:34:58 +0000

    Modified index.html, and web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+index = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!Tony ');
 });
 
 var port = process.env.PORT || 5000;
